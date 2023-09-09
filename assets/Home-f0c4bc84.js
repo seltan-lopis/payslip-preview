87f5ba75b5c22ec4b6ff56a83ba44c394ffb57da
seltan-lopis <seltanlopis98@gmail.com>
2023-09-09 13:58:41 +0530

    misc

---
+++ assets/Home-f0c4bc84.js
@@ -0,0 +1,1 @@
+import{_ as a,u as e,c as t,a as n,o as r,b as c}from"./index-a4460777.js";const _={data(){return{selected_file:null}},methods:{displayLoader(){e().display_loader=!0},hideLoader(){e().display_loader=!1},handleFileChange(o){},convertToJSON(){}}},d={class:""};function l(o,i,p,u,f,m){const s=c;return r(),t("div",d,[n(s)])}const x=a(_,[["render",l]]);export{x as default};
